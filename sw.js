a61750f9c4605d3299bb88be485685d6a5530133
gutomazzi <50110619+gutomazzi@users.noreply.github.com>
2025-09-22 19:37:39 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-self.addEventListener('fetch', () => {});
+//self.addEventListener('fetch', () => {});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Tenta buscar o arquivo na rede.
+        // Se a busca der certo, salva o arquivo no cache e o retorna.
+        return caches.open('meu-cache-dinamico').then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => {
+        // Se a busca na rede falhar, retorna a versão do cache.
+        return caches.match(event.request);
+      })
+  );
+});
